06ad4c8b60e430f086536460c04e99dc8fb770bf
Larche21 <burito0210@gmail.com>
2023-05-26 23:14:04 +0300

    6 version

--- core/src/ru/myitschool/ronin/ScreenGame3.java
+++ core/src/ru/myitschool/ronin/ScreenGame3.java
@@ -1,7 +1,5 @@
 package ru.myitschool.ronin;
 
-
-
 import static ru.myitschool.ronin.MyGG.SCR_HEIGHT;
 import static ru.myitschool.ronin.MyGG.SCR_WIDTH;
 
@@ -16,30 +14,41 @@ public class ScreenGame3 implements Screen {
     TextButton  btnBack;
 
     Texture imgRonin;
+    Texture imgSkel;
 
     Ronin ronin;
+    Skel skel;
     long timeStart;
 
-    float[] level = new float[]{110, 337, 670};
-    Ladder[] ladders = new Ladder[2];
+    float[] levels = new float[]{110, 337, 670}; // уровни экрана
+
+    Ladder[] ladders = new Ladder[2]; // лестницы экрана
+
+    Ladder[] abysses = new Ladder[2]; // пропасти
 
     public ScreenGame3(MyGG myGG) {
         gg = myGG;
-        imgGame1 = new Texture("Game1.png");
+        imgGame1 = new Texture("Game1.png"); // картинка экрана
         btnBack = new TextButton(gg.fontSmall, "Back", SCR_WIDTH-130, SCR_HEIGHT);
         imgRonin = new  Texture("Ronin.png");
+        imgSkel = new  Texture("Skel.png");
 
-        ronin = new Ronin(SCR_WIDTH/6f-100,level[1],130,144);
-        ladders[0] = new Ladder(561, 643, level[1], level[2]);
-        ladders[1] = new Ladder(1659, 1732, level[0], level[1]);
+        ronin = new Ronin(SCR_WIDTH/6f-100, levels[1],130,144);
+
+
+        ladders[0] = new Ladder(686, 754, levels[0], levels[1]); // лестницы экрана
+        ladders[1] = new Ladder(1290, 1362, levels[1], levels[2]); // лестницы экрана
+
+        skel = new Skel(SCR_WIDTH/6f+800, levels[2], 110,150);
+
+        abysses[0] = new Ladder(0, 888, levels[2], levels[2]);
+        abysses[1] = new Ladder(992, SCR_WIDTH, levels[0], levels[0]);
     }
 
 
     @Override
     public void show() {
-        timeStart = TimeUtils.millis();
-
-
+        timeStart =TimeUtils.millis();
     }
 
     @Override
@@ -48,7 +57,6 @@ public class ScreenGame3 implements Screen {
         if(Gdx.input.justTouched()) {
             gg.touch.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             gg.camera.unproject(gg.touch);
-            System.out.println(gg.touch.x +" "+ gg.touch.y);
             if(btnBack.hit(gg.touch.x, gg.touch.y)){
                 gg.setScreen(gg.screenIntro);
             } else {
@@ -72,19 +80,30 @@ public class ScreenGame3 implements Screen {
             }
         }
 
-
-
         // события
-        ronin.move(level);
-        for (int i = 0; i < level.length; i++) {
-            if(ronin.y == level[i]){
-                ronin.isOnLadder = false;
+        ronin.move(levels);
+        for (int i = 0; i < abysses.length; i++) {
+            if(ronin.x>abysses[i].x1 && ronin.x<abysses[i].x2 && abysses[i].y2==ronin.y){
+                ronin.vy = -10;
+                ronin.vx = 0;
+                ronin.isFalling = true;
             }
         }
+        if(ronin.isOnLadder) {
+            for (int i = 0; i < levels.length; i++) {
+                if (ronin.isOnLevel(levels[i])) {
+                    ronin.isOnLadder = false;
+                }
+            }
+        }
+        if(ronin.die()){
+            gg.setScreen(gg.screenIntro);
+        }
+
         // переходы на другие экраны
-        if(ronin.x < ronin.width/2){
-            gg.screenGame.ronin.x = SCR_WIDTH-gg.screenGame.ronin.width/2;
-            gg.screenGame.ronin.y = ronin.y;
+        if(ronin.x > SCR_WIDTH-ronin.width/2){
+            gg.screenGame2.ronin.x = gg.screenGame2.ronin.width/2;
+            gg.screenGame2.ronin.y = ronin.y;
             ronin.targetX = ronin.x-ronin.vx;
             gg.setScreen(gg.screenGame2);
         }
@@ -96,6 +115,7 @@ public class ScreenGame3 implements Screen {
         gg.batch.draw(imgGame1, 0, 0, SCR_WIDTH, SCR_HEIGHT);
         btnBack.font.draw(gg.batch, btnBack.text, btnBack.x, btnBack.y);
         gg.batch.draw(imgRonin, ronin.scrX(), ronin.scrY(), ronin.width, ronin.height, 0, 0, 130, 144, ronin.direction, false);
+        gg.batch.draw(imgSkel, skel.scrX() , skel.scrY(), skel.width , skel.height);
         gg.batch.end();
     }
     @Override
@@ -123,3 +143,5 @@ public class ScreenGame3 implements Screen {
         imgGame1.dispose();
     }
 }
+
+
